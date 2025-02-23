caa4c01e26a563fedc80a7d31e9ce13cf332ed95
Gustavo Gaspar <glgaspar@live.com>
2025-02-23 01:27:30 -0300

    setting up routes

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/glgaspar/pay_checker_api/controller"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("Error loading .env file: %s", err)
+	}
+
+	router := gin.Default()
+
+	setUpRoutes(router)
+
+	router.Run("0.0.0.0:8080")
+}
+
+func setUpRoutes(router *gin.Engine) {
+	// healthcheck
+	router.GET("/", func(c *gin.Context) { c.Done() })
+
+	// create new bill
+	router.POST("/", controller.CreateBill)
+
+	// update bill data
+	router.POST("/udpate", controller.UpdateBill)
+	router.POST("/pay", controller.PayBill)
+
+	// list bills
+	router.GET("/list", controller.GetList)
+}
